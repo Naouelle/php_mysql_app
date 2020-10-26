27273ff36887d2a4feb1d6d17da84cc0fc720f78
emma b <buscher.emmanuelle@orange.fr>
2020-10-26 23:18:37 +0100

    variables_constantes

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
-phpinfo();
+define('DB', 'database');
 
-?>
+echo constant('DB');
+
+$test = null;
+if (isset($test)) {
+    echo 'ok';
+} else {
+    echo 'ko';
+}
+
+$test = null;
+if (empty($test)) {
+    echo 'ok';
+} else {
+    echo 'ko';
+}
+
+$test = null;
+if (!empty($test)) {
+    echo 'ok';
+} else {
+    echo 'ko';
+}
+unset($test);
+
+var_dump($test);
+die();
